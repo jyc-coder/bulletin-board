4596c06610e540cab71433fd661f44f3f0b12e92
jyc-coder <jyc4648@gmail.com>
2022-08-12 14:42:32 +0900

    #13 - AuditingFields 추출
    생성자,생성일시,수정사,수정일시는 반복적으로
    엔티티 클래스에 들어가는 요소이고,
    도메인과 직접 연관이 없는 요소이므로 추출이 가능하다. 
    
    `@MappedSuperclass`이용하여 상속 방식으로 추출함
    그외 `@DateTimeFormat`요소 추가 및 jpa 옵션 개선

--- src/main/java/com/jycproject/bulletinboard/domain/ArticleComment.java
+++ src/main/java/com/jycproject/bulletinboard/domain/ArticleComment.java
@@ -22,9 +22,9 @@ import java.util.Objects;
         @Index(columnList ="createdBy")
 })
 
-@EntityListeners(AuditingEntityListener.class)
+
 @Entity
-public class ArticleComment {
+public class ArticleComment extends AuditingFields {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
@@ -33,10 +33,6 @@ public class ArticleComment {
     @Setter @Column(nullable = false, length = 500 )private String content; // 본문
 
 
-    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt; // 생성일시
-    @CreatedBy @Column(nullable = false, length = 100)private String createdBy; // 생성자
-    @LastModifiedDate @Column(nullable = false)private LocalDateTime modifiedAt; // 수정일시
-    @LastModifiedBy @Column(nullable=false, length = 100)private String modifiedBy; // 수정자
 
     protected ArticleComment() {
     }
@@ -49,7 +45,6 @@ public class ArticleComment {
     public static ArticleComment of(Article article, String content) {
        return new ArticleComment(article,content);
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;

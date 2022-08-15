9d0019c2f514164259de369fed91dffb3d9898e6
jyc-coder <jyc4648@gmail.com>
2022-08-15 20:18:32 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.  현재는 데이터가 존재하지 않으므로 실패하는 테스트이기 때문에 `gradle build` 실패로 배포 자동화에 악영향을 미치는 경우가 있으므로 실패 테스트는 테스트 제외 처리하는 방향으로 정책을 잡아보기로 했다.
    이에 기능 구현 중인 유닛 테스트를 모두 `@disabled` 처리했다.

---
+++ src/main/java/com/jycproject/bulletinboard/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.jycproject.bulletinboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/*
+/articles
+/articles/{article-id}
+/articles/search
+/articles/search-hashtag
+ */
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}

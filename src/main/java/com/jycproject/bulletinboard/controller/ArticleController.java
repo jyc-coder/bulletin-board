078a558e9c84165dc3ea89442ff63121a7c44eaf
jyc-coder <jyc4648@gmail.com>
2022-08-16 14:58:32 +0900

    #17 - 게시판 뷰 기본 골격 작성
    컨트롤러 랜들러 메소드로 테스트를 통과하게 열어주고 뷰 적절히 뼈대 잡음

--- src/main/java/com/jycproject/bulletinboard/controller/ArticleController.java
+++ src/main/java/com/jycproject/bulletinboard/controller/ArticleController.java
@@ -1,7 +1,12 @@
 package com.jycproject.bulletinboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 /*
 /articles
 /articles/{article-id}
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 }
--- src/test/java/com/jycproject/bulletinboard/controller/ArticleControllerTest.java
+++ src/test/java/com/jycproject/bulletinboard/controller/ArticleControllerTest.java
@@ -33,7 +33,7 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/index")) // 뷰의 존재여부 검사
                 .andExpect(model().attributeExists("articles")); // 뷰에 모델 어트리뷰트로 넣어준 데이터존재 여부 검사
     }
-    @Disabled("구현 중")
+
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {

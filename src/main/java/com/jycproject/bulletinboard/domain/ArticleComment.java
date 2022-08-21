88a516f1b5c5f14684853eca0115f4b93846109e
jyc-coder <jyc4648@gmail.com>
2022-08-21 16:22:25 +0900

    #23 - 회원 계정 엔티티와 게시글,댓글에 연관관계 적용
    테이블 필드 수정으로 영향 받은 `data.sql`테스트 데이터도 모두 업데이트
    게시글 -> 댓글컬렉션의 정렬기준을 최신순으로 적용 (`createdBy` DESC)

--- src/main/java/com/jycproject/bulletinboard/domain/ArticleComment.java
+++ src/main/java/com/jycproject/bulletinboard/domain/ArticleComment.java
@@ -3,6 +3,7 @@ package com.jycproject.bulletinboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.catalina.User;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
 
         @Index(columnList ="content"),
@@ -29,7 +30,10 @@ public class ArticleComment extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     @Setter private Long id;
+
     @Setter private @ManyToOne(optional = false) Article article; // 게시글 (ID)
+    @Setter private @ManyToOne(optional = false) UserAccount userAccount; // 유저 정보(ID)
+
     @Setter @Column(nullable = false, length = 500 )private String content; // 본문
 
 
@@ -37,13 +41,14 @@ public class ArticleComment extends AuditingFields {
     protected ArticleComment() {
     }
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article,UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-       return new ArticleComment(article,content);
+    public static ArticleComment of(Article article,UserAccount userAccount, String content) {
+       return new ArticleComment(article,userAccount,content);
     }
     @Override
     public boolean equals(Object o) {

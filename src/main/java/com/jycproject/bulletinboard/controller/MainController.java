078b76d9cd88ef1aad561d103452c85744aeb75b
jyc-coder <jyc4648@gmail.com>
2022-08-22 10:28:53 +0900

    #22 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현
    `MainController`를 만들어서 기본 페이지가 게시판 페이지가 되도록 했다.
    redirect로 접근하는 것은 단순 조회 접근이고, url 루트 경로를 유지한 채로 게시판 페이지를 대표 페이지로 만들고 싶기 때문에 forward로 사용했다.

---
+++ src/main/java/com/jycproject/bulletinboard/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.jycproject.bulletinboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String root() {
+        return "forward:/articles";
+    }
+}

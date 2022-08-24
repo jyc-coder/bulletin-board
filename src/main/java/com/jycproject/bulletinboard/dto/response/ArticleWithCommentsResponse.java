5ffaa17ba4005ad2c74674bd9cd4b98c4cddd445
jyc-coder <jyc4648@gmail.com>
2022-08-24 14:47:05 +0900

    #22 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러를 구현하고 데이터 전달완료
    원활한 데이터 전달을 위해 `ArticleWithCommentREsponse`dto를 생성함
    게시판 페이지 기본 정렬 속성 부여 : 페이지당 10개, 시간 내림차순

---
+++ src/main/java/com/jycproject/bulletinboard/dto/response/ArticleWithCommentsResponse.java
@@ -0,0 +1,45 @@
+package com.jycproject.bulletinboard.dto.response;
+
+import com.jycproject.bulletinboard.dto.ArticleWithCommentsDto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record ArticleWithCommentsResponse(
+        Long id,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String email,
+        String nickname,
+        Set<ArticleCommentResponse> articleCommentsResponse
+
+) implements Serializable {
+
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponse) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponse);
+    }
+    public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto){
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+        return new ArticleWithCommentsResponse(
+                dto.id(),
+                dto.title(),
+                dto.content(),
+                dto.hashtag(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname,
+                dto.articleCommentDtos().stream()
+                        .map(ArticleCommentResponse::from)
+                        .collect(Collectors.toCollection(LinkedHashSet::new))
+        );
+    }
+
+}

d1f1262c59232abfd7bd6c00d34807d2effd622d
jyc-coder <jyc4648@gmail.com>
2022-08-26 09:04:36 +0900

    #28 - 로그인 테스트 문제 해결
    all 테스트를 돌려봤는데 View 컨트롤러 인증 테스트가 실패해서 살펴보고 문제 해결
    `/login`페이지는 직접 만든 컨트롤러가 아니고 스프링 시큐리티가 대신 해주는 작업이므로, 컨트롤러 테스트에서 읽어야 할 컨트롤러 빈이 없다
    따라서 `Void.class`로 변경하고 다시 테스트를 돌리니 통과되었다.

--- src/test/java/com/jycproject/bulletinboard/controller/AuthControllerTest.java
+++ src/test/java/com/jycproject/bulletinboard/controller/AuthControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -32,7 +33,8 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk()) // 정상 호출
-                .andExpect(result -> content().contentType(MediaType.TEXT_HTML)); // 데이터 확인
+                .andExpect(result -> content().contentType(MediaType.TEXT_HTML)) // 데이터 확인
+                .andDo(MockMvcResultHandlers.print());
 
     }
 }
